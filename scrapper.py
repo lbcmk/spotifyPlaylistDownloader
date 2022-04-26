e36a90988db94b111a81bd7d0429c75f7be64bfa
LotusDeath69 <LotusDeath69@gmail.com>
2022-04-26 14:47:54 -0400

    v1

--- scrapper.py
+++ scrapper.py
@@ -1,11 +1,3 @@
-"""
-Variables Section 
-
-"""
-SPOTIFY_API_KEY = ""
-SPOTIFY_PLAYLIST_ID = ""
-
-
 from selenium import webdriver
 import selenium
 from selenium.webdriver.chrome.options import Options
@@ -25,10 +17,18 @@ from slugify import slugify
 load_dotenv()
 
 
+"""
+Variables Section 
+
+"""
+SPOTIFY_PLAYLIST_ID = "0Hx4aQ2xTb4TKl5Z56DJh0"
+SPOTIFY_API_KEY = os.environ["API"]
+
+
 class bot:
-    def __init__(self):
+    def __init__(self, key, playlist_id, n=0):
         self.download_list = [] 
-        self.get_tracks()
+        self.get_tracks(key, playlist_id)
         options = webdriver.ChromeOptions()
         prefs = {"profile.default_content_settings.popups": 0,
              "download.default_directory": r"C:\Users\lotus\Downloads\temp", #IMPORTANT - ENDING SLASH V IMPORTANT
@@ -38,12 +38,13 @@ class bot:
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         options.add_experimental_option("prefs", prefs)
         self.driver = webdriver.Chrome(options=options)
-        self.wait = WebDriverWait(self.driver, 15)
+        self.wait = WebDriverWait(self.driver, 60)
+        self.n = n
         self.start()
         
         
     def start(self):
-        for self.number, i in enumerate(self.download_list):
+        for self.number, i in enumerate(self.download_list[self.n-1:]):
             self.driver.get(f"https://www.youtube.com/results?search_query={i}")
             element = self.find(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/div/div[1]/div/h3/a")
             link = f'{element.get_attribute("href")[:19]}pi{element.get_attribute("href")[19:]}'
@@ -79,7 +80,7 @@ class bot:
             sleep(0.5)
             onlyfiles = [f for f in listdir(r"C:\Users\lotus\Downloads\temp") if isfile(join(r"C:\Users\lotus\Downloads\temp", f))]
         old_file = os.path.join(r"C:\Users\lotus\Downloads\temp", onlyfiles[0])
-        new_file = os.path.join(r"C:\Users\lotus\Downloads\temp\playlist", f"{self.number+1}. {slugify(self.song_name)}.mp3")
+        new_file = os.path.join(r"C:\Users\lotus\Downloads\temp\playlist", f"{self.number+self.n}. {slugify(self.song_name)}.mp3")
         os.replace(old_file, new_file)
         
         
@@ -88,11 +89,11 @@ class bot:
         return self.driver.find_element(by, element)
     
     
-    def get_tracks(self):
-        total = requests.get("https://api.spotify.com/v1/playlists/0Hx4aQ2xTb4TKl5Z56DJh0/tracks?market=CA&fields=total", headers={"Content-Type": "application/json", "Authorization": f"Bearer {os.environ['API']}"}).json()['total']        
+    def get_tracks(self, key, id):
+        total = requests.get(f"https://api.spotify.com/v1/playlists/{id}/tracks?market=CA&fields=total", headers={"Content-Type": "application/json", "Authorization": f"Bearer {key}"}).json()['total']        
         for n in range(divmod(total, 100)[0] + 1):
-            res = requests.get(f"https://api.spotify.com/v1/playlists/0Hx4aQ2xTb4TKl5Z56DJh0/tracks?market=CA&fields=items(track.name%2Ctrack.artists.name)&offset={n*100}", headers={"Content-Type": "application/json", "Authorization": f"Bearer {os.environ['API']}"}).json()    
+            res = requests.get(f"https://api.spotify.com/v1/playlists/{id}/tracks?market=CA&fields=items(track.name%2Ctrack.artists.name)&offset={n*100}", headers={"Content-Type": "application/json", "Authorization": f"Bearer {key}"}).json()    
             for i in res["items"]:
                 self.download_list.append(f'{i["track"]["name"]} by {i["track"]["artists"][0]["name"]} lyrics')
         
-v1 = bot() 
+v1 = bot(SPOTIFY_API_KEY, SPOTIFY_PLAYLIST_ID) 
